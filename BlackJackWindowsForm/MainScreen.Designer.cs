b6c96f2752ffd79eff338a53bfd01cf5d7bef23f
lembap10 <baptiste.lemercier10@gmail.com>
2022-01-07 13:21:56 -0600

    Add project files.

---
+++ BlackJackWindowsForm/MainScreen.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace BlackJackWindowsForm
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::BlackJackWindowsForm.Properties.Resources.Green_Green_PO;
+            this.ClientSize = new System.Drawing.Size(1834, 861);
+            this.Name = "MainScreen";
+            this.RightToLeftLayout = true;
+            this.Text = "BlackJack App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
